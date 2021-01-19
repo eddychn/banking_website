d1b8ae01ae5ea9dd6013b92affd0c73f35b99f13
eddychn <adtchauhan123@gmail.com>
2021-01-19 20:40:08 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+const userNameE1 = document.getElementById('userName');
+const userEmailE1 = document.getElementById('userEmail');
+
+function loginInfo() {
+    
+    if(userNameE1.value==''){
+        
+        alert('Login First😕😕')
+    }else{
+        alert('You have successfully logged in!!😴🎉🎉')
+        //console.log('hello')
+        
+    }
+    
+    
+}
